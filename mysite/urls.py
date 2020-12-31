753c4a90b6c52ae1e63ef1824910dcf00742e12c
tomiswinner <mouseofsack911@gmail.com>
2020-12-31 10:58:44 +0900

    4th for nothing

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('blog.urls')),
 ] + static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
